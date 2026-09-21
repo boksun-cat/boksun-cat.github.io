ae7f31c6582d796ce5e172ba5944e06462b66c1e
pouz <whitewyrm84@gmail.com>
2026-09-21 12:29:52 +0900

    fix: set dynamic OAuth redirect URL to current web host

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2535928172" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2695010695" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
